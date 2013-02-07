17a9da3075b3e0ac177599951b584de8e6129b94
armangal <arman@armangal.com>
2013-02-08 00:58:29 +0200

    sort

--- src/com/smexec/monitor/client/servers/ServersWidget.java
+++ src/com/smexec/monitor/client/servers/ServersWidget.java
@@ -1,6 +1,8 @@
 package com.smexec.monitor.client.servers;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
@@ -19,6 +21,16 @@ public class ServersWidget
         getScrollPanel().clear();
         FlowPanel fp = new FlowPanel();
         getScrollPanel().add(fp);
+        Collections.sort(list, new Comparator<ConnectedServer>() {
+
+            @Override
+            public int compare(ConnectedServer o1, ConnectedServer o2) {
+                if (o1.getStatus() && o2.getStatus()) {
+                    return o1.getName().compareTo(o2.getName());
+                }
+                return o2.getStatus().compareTo(o1.getStatus());
+            }
+        });
         for (ConnectedServer cs : list) {
             fp.add(new HTML(cs.getIp() + ":" + cs.getJmxPort() + (cs.getStatus() ? " Connected" : " <b>Offline</b>") + " >> [" + cs.getName() + "]"));
         }
