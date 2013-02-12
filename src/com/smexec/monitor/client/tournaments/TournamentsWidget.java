221186e2048dd241f9a6a39e98dc82b2fefef9c6
armangal <arman@armangal.com>
2013-02-12 12:55:21 +0200

    more

--- src/com/smexec/monitor/client/tournaments/TournamentsWidget.java
+++ src/com/smexec/monitor/client/tournaments/TournamentsWidget.java
@@ -1,12 +1,36 @@
 package com.smexec.monitor.client.tournaments;
 
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.smexec.monitor.client.widgets.AbstractMonitoringWidget;
 
 public class TournamentsWidget
     extends AbstractMonitoringWidget {
 
+    private FlowPanel fp = new FlowPanel();
+
     public TournamentsWidget() {
         super("Tournaments");
         setStyleName("tournamentsWidget");
+
+        getScrollPanel().add(fp);
+    }
+
+    public void update() {
+        fp.clear();
+        FlowPanel left = getLeft();
+        FlowPanel right = getRight();
+        fp.add(left);
+        fp.add(right);
+
+    }
+
+    private FlowPanel getLeft() {
+        FlowPanel left = new FlowPanel();
+        return left;
+    }
+
+    private FlowPanel getRight() {
+        FlowPanel left = new FlowPanel();
+        return left;
     }
 }
