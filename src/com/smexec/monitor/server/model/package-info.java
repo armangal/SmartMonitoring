5dbd5b2395bc64b0f43eb8979a31cc9b17fc8ebc
armangal <arman@armangal.com>
2013-03-11 16:14:23 +0200

    maven

--- src/com/smexec/monitor/server/model/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.armangal.com/smartexecutormonitor/v1")
-package com.smexec.monitor.server.model;
--- src/com/smexec/monitor/server/tasks/ConnectionSynch.java
+++
@@ -1,8 +0,0 @@
-package com.smexec.monitor.server.tasks;
-
-import java.util.concurrent.locks.ReentrantLock;
-
-public class ConnectionSynch {
-
-    public static ReentrantLock connectionLock = new ReentrantLock();
-}
