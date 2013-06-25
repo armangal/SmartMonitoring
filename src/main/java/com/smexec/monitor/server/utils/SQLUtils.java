c752636b333af25dd11ff36fd6ecc82658dcc7b3
armangal <arman@armangal.com>
2013-06-25 16:15:06 +0300

    1.0.31

---
+++ src/main/java/com/smexec/monitor/server/utils/SQLUtils.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2013 Arman Gal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.smexec.monitor.server.utils;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SQLUtils {
+
+    static Logger logger = LoggerFactory.getLogger("SQLUtils");
+
+    /** Close resultset and do not throw an exception */
+    public static void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            logger.error("Exception on closing SQL Result set: " + e.getMessage(), e);
+        }
+    }
+}
